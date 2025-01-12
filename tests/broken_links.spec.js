eaeea2f45f65424956d2befa2a7370226b4d7849
Sunny <suri.sunny0@gmail.com>
2025-01-12 16:40:23 +0530

    did some changes in broken_links script

--- tests/broken_links.spec.js
+++ tests/broken_links.spec.js
@@ -1,41 +1,51 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 async function get_all_links(page) {
-    const links = await page.$$("a");
-    const hrefs = await Promise.all(
-        links.map((link) => link.getAttribute("href"))
-    );
+
+    const links = await page.$$('a');
+    const hrefs = await Promise.all(links.map((link) => link.getAttribute("href")))
 
     const valid_hrefs = hrefs.reduce((acc, href) => {
-        if (href && href.includes("https")) {
+
+        if (href && href.startsWith("https")) {
             acc.add(href);
+
         }
         return acc;
-    }, new Set());
+
+    }, new Set())
 
     return Array.from(valid_hrefs);
+
 }
 
-test("Get all broken links from page", async ({ page }) => {
-    await page.goto('https://ecommerce-playground.lambdatest.io/');
+test("broken links", async ({ page }) => {
 
+    await page.goto("https://ecommerce-playground.lambdatest.io/index.php?route=common/home");
     const links = await get_all_links(page);
+    console.log(links.length);
 
-    for (let url of links) {
-        let statusCode = 0;
+    await Promise.all(links.map(async (url) => {
         try {
-            const resp = await page.request.get(url, { failOnStatusCode: false });
-            statusCode = resp.status();
-
-            if (statusCode >= 400) {
-                expect.soft(false, `${finalUrl} is not ok (Status: ${statusCode})`).toBe(true);
+            const resp = await page.request.get(url);
+            if (resp.ok()) {
+                expect.soft(true, `${url} is working perfectly fine`).toBeTruthy();
             } else {
-                expect.soft(true, `${finalUrl} is ok (Status: ${statusCode})`).toBe(true);
+                expect.soft(false, `${url} is not working`).toBeTruthy();
+
             }
+
         } catch (error) {
-            console.error(`Error checking link ${finalUrl}:`, error);
-            console.error(`Status code: ${statusCode}`);
-            expect.soft(false, `${finalUrl} is not ok (Status: ${statusCode})`).toBe(true);
+
+            console.log(`Error checking ${url}`, error);
+
+
         }
-    }
-});
+    }))
+
+
+
+
+
+
+})
