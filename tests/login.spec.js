3b4462461dd41b3bf2499ec829fd4ee6c25fbd49
Sunny <suri.sunny0@gmail.com>
2025-01-12 16:38:22 +0530

    did some changes in login script

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,47 +1,40 @@
 import { test, expect } from "@playwright/test";
 
-test.use({ viewport: { width: 1400, height: 700 } });
+// test.use({ viewport: { width: 1400, height: 700 } });
 
 test("verify login functionality", async ({ page }) => {
   await page.goto(
-    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    "https://nearform.github.io/testing-playground/#/login-form"
   );
 
-  await page.getByPlaceholder("Username").fill("Admin");
-  await page.getByPlaceholder("Password").fill("admin123");
-  await page.locator("button[type='submit']").click();
+  await page.getByLabel("Username").fill("admin");
+  await page.getByLabel("Password").fill("Passw0rd!");
+  await page.getByTestId("login-button").click();
 
-  await expect(page).toHaveURL(/dashboard/);
+  await expect(page.getByText("You are currently logged in!")).toBeVisible();
+  await expect(page.getByText("You are currently logged in!")).toHaveText("You are currently logged in!")
 
-  // await expect(page.locator('//h6[normalize-space()="Dashboard"]')).toHaveText(
-  //   "Dashboard"
-  // );
-  // await expect(page).toHaveText(
-  //   "Dashboard"
-  // );
-  // await expect(page.locator('//h6[normalize-space()="Dashboard"]')).toContainText(
-  //   "Dashboard"
-  // );
+ await page.getByText("You are currently logged in!").screenshot({path:"./text.png"});
 
-  // await page
-  //   .locator('//h6[normalize-space()="Dashboard"]')
-  //   .screenshot({ path: "./screenshot.png" });
+
+
 });
 
-test("verify logout functionality", async ({ page }) => {
+test.only("verify logout functionality", async ({ page }) => {
   await page.goto(
-    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    "https://nearform.github.io/testing-playground/#/login-form"
   );
 
-  await page.getByPlaceholder("Username").fill("Admin");
-  await page.getByPlaceholder("Password").fill("admin123");
-  await page.locator("button[type='submit']").click();
+  await page.getByLabel("Username").fill("admin");
+  await page.getByLabel("Password").fill("Passw0rd!");
+  await page.getByTestId("login-button").click();
 
-  await page.locator("//img[@class='oxd-userdropdown-img']").click();
+  await page.getByTestId("logout-button").click();
 
-  await page.waitForTimeout(5000);
+  await expect(page.getByLabel("Username")).toBeVisible();
 
-  await page.locator("//a[normalize-space()='Logout']").click();
 
-  await expect(page).toHaveURL(/login/);
+
+
+
 });
